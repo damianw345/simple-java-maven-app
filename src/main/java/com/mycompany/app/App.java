b1ca059393282028d475131c34f198dc0bb96261
damianw345 <damianw345@gmail.com>
2018-11-27 20:11:15 +0100

    A

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello Woaaaaarlda!";
+    private final String message = "Hello World!";
 
     public App() {}
 
